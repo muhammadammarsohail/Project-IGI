2af17e8cfefc35523f2abece5803fa7a85ed014d
Ammar Sohail <muhammadammarsohail@gmail.com>
2019-04-28 18:46:49 +0500

    Add base Person with a method hurt and a child Opponent class.

---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGI
+{
+    class Person
+    {
+        private double life = 100;
+        private double attack;
+        private readonly double defence;
+        string name;
+
+        public double hurt(int attacked)
+        {
+            double decLife = attacked * defence;
+            life -= decLife;
+            return decLife;
+        }
+    }
+}
